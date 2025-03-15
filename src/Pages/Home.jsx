440691d22f45fe28d0752c407cd92d688bf5f281
chaudhary64 <chiragchoudhary64@gmail.com>
2025-03-15 22:33:48 +0530

    feat: add step-by-step guide section to Home component with images

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -45,9 +45,30 @@ const Home = () => {
     },
   ];
 
+  const steps = [
+    {
+      id: 1,
+      src: "/assets/images/step1.png",
+      title: "Pick a CV template.",
+      description: "Choose a sleek design and layout to get started.",
+    },
+    {
+      id: 2,
+      src: "/assets/images/step2.png",
+      title: "Fill in the blanks.",
+      description: "You'll fill in the blanks with your information.",
+    },
+    {
+      id: 3,
+      src: "/assets/images/step3.png",
+      title: "Download your CV.",
+      description: "You'll get a downloadable file in your inbox.",
+    },
+  ];
+
   return (
     <main>
-      <div className="text-center my-28">
+      <section className="text-center my-28">
         <p className="text-6xl">Create a proffesional Resume</p>
         <div className="my-6 text-lg text-gray-600">
           <p>Fill in the blanks, choose a template and download your</p>
@@ -56,8 +77,26 @@ const Home = () => {
         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded transition-colors duration-200">
           Get Started
         </button>
-      </div>
+      </section>
       <Swipper data={swippeCards} />
+      <section className="my-24">
+        <p className="text-center text-4xl">
+          How ResumePro resume builder works
+        </p>
+        <div className="my-20 flex justify-evenly">
+          {steps.map((step) => (
+            <div key={step.id}>
+              <img src={step.src} alt={step.title} />
+              <p className="text-center font-[500] text-2xl mt-4">
+                {step.title}
+              </p>
+              <p className="mt-1 text-base text-center text-gray-900">
+                {step.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
