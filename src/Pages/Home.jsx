00d7c7d74bb17922e90b8a52bde9aa18375ef5ce
chaudhary64 <chiragchoudhary64@gmail.com>
2025-03-15 22:58:54 +0530

    feat: add new section to Home component for CV transformation with background image and checkmarks

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -97,6 +97,40 @@ const Home = () => {
           ))}
         </div>
       </section>
+      <section
+        style={{
+          backgroundImage: `url("/assets/images/changeCV-bg-img.png")`,
+          backgroundSize: "cover",
+        }}
+        className="flex justify-evenly items-center py-28"
+      >
+        <img src="/assets/images/changeCV-image.webp" alt="" />
+        <div className="w-1/2">
+          <p className="text-4xl">Transform Your CV. Transform Your Career.</p>
+          <p className="my-4 text-lg">
+            Elevate your professional profile with ResumePro CV Builder—an
+            advanced career development tool designed to help you stand out. If
+            your current CV lacks impact, it's time for an upgrade. Explore our
+            expertly crafted templates and make a lasting impression.
+          </p>
+          {/* Checkmarks */}
+          <div className="my-6">
+            {[
+              "Leverage tailored recommendations to create a polished CV in minutes.",
+              "Impress recruiters with sophisticated, modern designs.",
+              "Let employers see your true self—the best candidate for the job.",
+            ].map((item, index) => (
+              <div key={index} className="flex gap-3 my-4 text-lg">
+                <img src="/assets/images/circle-check.png" alt="" />
+                <p>{item}</p>
+              </div>
+            ))}
+          </div>
+          <button className="bg-[#FBAF38] font-bold py-3 px-8 rounded-full">
+            Creat your CV Now
+          </button>
+        </div>
+      </section>
     </main>
   );
 };
